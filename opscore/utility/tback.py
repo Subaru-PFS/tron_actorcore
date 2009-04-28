f51fb4a7f01adc4654da8751f9b0b41cde98b754
cloomis <cloomis@b00c9580-4927-0410-8824-811d448ea4c7>
2009-04-28 18:12:13 +0000

    add basic traceback formatter
    git-svn-id: svn+ssh://sdss3.org/repo/ops/opscore/trunk@99779 b00c9580-4927-0410-8824-811d448ea4c7

---
+++ opscore/utility/tback.py
@@ -0,0 +1,40 @@
+__all__ = ['tback']
+
+import inspect
+import pprint
+import sys
+import traceback
+
+import logging
+
+def tback(system, e, info=None, logger=None):
+        """ Log a decently informative traceback. """
+
+        if logger == None:
+            logger = logging
+            
+        try:
+            frames = inspect.trace()
+            toptrace = inspect.trace()[-1]
+        except:
+            one_liner = "%s: %s: %s" % (e, sys.exc_type, sys.exc_value)
+            logger.critical("======== %s exception botch: %s" % (system, one_liner))
+            return
+        
+        tr_list = []
+        tr_list.append("\n\n====== trace:\n")
+        tr_list.append(pprint.pformat(toptrace))
+        
+        i = 0
+        frames.reverse()
+        for f in frames:
+            #tr_list.append("\n\n====== frame %d arg+local names:\n" % (i))
+            #tr_list.append(pprint.pformat(f[0].f_code.co_varnames))
+            tr_list.append("\n\n====== frame %d locals:\n" % (i))
+            tr_list.append(pprint.pformat(f[0].f_locals))
+            i += 1
+
+        ex_list = traceback.format_exception(sys.exc_type, sys.exc_value, sys.exc_traceback)
+        logger.warn("\n======== %s exception: %s\n" % (system, ''.join(ex_list)))
+        logger.warn("\n======== %s exception details: %s\n" % (system, ''.join(tr_list)))
+        
