d06678ee8a321d3ffb611f19a1cf49a35aaef163
Craig Loomis <cloomis@astro.princeton.edu>
2013-04-24 13:01:25 -0400

    close to a complete template?

---
+++ templateActor/python/__PRODUCT_NAME__/Commands/TopCmd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import opscore.protocols.keys as keys
+import opscore.protocols.types as types
+from opscore.utility.qstr import qstr
+
+class TopCmd(object):
+
+    def __init__(self, actor):
+        # This lets us access the rest of the actor.
+        self.actor = actor
+
+        # Declare the commands we implement. When the actor is started
+        # these are registered with the parser, which will call the
+        # associated methods when matched. The callbacks will be
+        # passed a single argument, the parsed and typed command.
+        #
+        self.vocab = [
+            ('ping', '', self.ping),
+            ('status', '', self.status),
+        ]
+
+        # Define typed command arguments for the above commands.
+        self.keys = keys.KeysDictionary("mcs_mcs", (1, 1),
+                                        )
+
+
+    def ping(self, cmd):
+        """Query the actor for liveness/happiness."""
+
+        cmd.warn("text='I am an empty and fake actor'")
+        cmd.finish("text='Present and (probably) well'")
+
+    def status(self, cmd):
+        """Report camera status and actor version. """
+
+        self.actor.sendVersionKey(cmd)
+        
+        cmd.inform('text="Present!"')
+        cmd.finish()
+
