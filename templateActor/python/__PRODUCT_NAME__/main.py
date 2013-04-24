7257bf6167de4b56ad5876fbf8c029d413b81b76
Craig Loomis <cloomis@astro.princeton.edu>
2013-04-24 13:06:26 -0400

    make the names a bit more generic

--- templateActor/python/__PRODUCT_NAME__/main.py
+++ templateActor/python/__PRODUCT_NAME__/main.py
@@ -2,7 +2,7 @@
 
 import actorcore.Actor
 
-class __ACTOR_NAME__(actorcore.Actor.Actor):
+class OurActor(actorcore.Actor.Actor):
     def __init__(self, name, productName=None, configFile=None, debugLevel=30):
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
@@ -13,11 +13,8 @@ class __ACTOR_NAME__(actorcore.Actor.Actor):
 #
 # To work
 def main():
-    theActor = __ACTOR_NAME__('__ACTOR_NAME__', productName='__PRODUCT_NAME__')
+    theActor = OurActor('__ACTOR_NAME__', productName='__PRODUCT_NAME__')
     theActor.run()
 
 if __name__ == '__main__':
     main()
-
-    
-    
