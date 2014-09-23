d7b330a2b7dd25cbe7109dcb2194aa589eb88609
Craig Loomis <cloomis@astro.princeton.edu>
2014-09-23 14:54:44 -0400

    Add some QThread tests.

---
+++ tests/qthread.py
@@ -0,0 +1,71 @@
+import time
+import unittest
+import Queue
+
+import actorcore.QThread
+
+class TestException(Exception):
+    pass
+
+class TestLogic(object):
+    def __init__(self):
+        self.stack = []
+
+    def countMsg(self, n=1):
+        return(range(n))
+
+    def raiseMesg(self):
+        raise TestException('kaboom')
+
+    def pushMsg(self, item=None):
+        self.stack.append(item)
+        print("pushed %s onto %s" % (item, self.stack))
+        
+    def clearMsg(self):
+        ret = self.stack
+        self.stack = []
+
+        return ret
+
+class TestQThread(unittest.TestCase):
+    def setUp(self):
+        self.testLogic = TestLogic()
+        self.testQ = actorcore.QThread.QThread(None, 'test_thread', timeout=2)
+        self.testQ.startUp()
+
+    def tearDown(self):
+        self.testQ.exit()
+        del self.testQ
+
+    def test_call(self):
+        n = 10
+        ret = self.testQ.call(self.testLogic.countMsg, n=n)
+        self.assertEqual(ret, range(n))
+
+    def test_raise(self):
+        with self.assertRaises(TestException):
+            self.testQ.call(self.testLogic.raiseMesg)
+
+    def test_thread1(self):
+        n = 1000
+        for i in range(n):
+            self.testQ.putMsg(self.testLogic.pushMsg, item=i)
+        ret1 = self.testQ.call(self.testLogic.clearMsg)
+        self.assertEqual(ret1, range(n))
+
+        for i in range(2):
+            self.testQ.putMsg(self.testLogic.pushMsg, item=i)
+        ret2 = self.testQ.call(self.testLogic.clearMsg)
+
+        self.assertEqual(ret2, range(2))
+
+def main(args=None):
+    if isinstance(args, basestring):
+        import shlex
+        args = shlex.split(args)
+
+    unittest.main(verbosity=9, failfast=True)
+
+if __name__ == "__main__":
+    main()
+
