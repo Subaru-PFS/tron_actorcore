37a3a4be6b61a51634f6ad5c6b5bf8f7c8325429
Craig Loomis <cloomis@astro.princeton.edu>
2014-10-22 14:19:19 -0400

    add error message of listen() failure

--- python/actorcore/CommandLinkManager.py
+++ python/actorcore/CommandLinkManager.py
@@ -34,8 +34,13 @@ class CommandLinkManager(Factory):
         self.activeConnections = []
         self.connID = 1
 
-        self.listeningPort = reactor.listenTCP(port, self, interface=interface)
-        
+        try:
+            self.listeningPort = reactor.listenTCP(port, self, interface=interface)
+        except Exception, e:
+            logging.error("Cannot listen for hub connections: %s", e)
+            logging.error("Check that the host and port in the configuration file is not used by another program:")
+            raise
+
     def fetchCid(self):
         """ Return the next available connection ID. """
 
