0852e5a67c40c6be6e3331246f7920ce6314b34c
Craig Loomis <cloomis@astro.princeton.edu>
2014-10-24 20:05:55 -0400

    Attempt to placate picky sdss3tools.setup

--- setup.py
+++ setup.py
@@ -9,4 +9,9 @@ import sdss3tools
 sdss3tools.setup(
     description = "Common code base for PFS MHS actor system",
     name = "ics_mhs_actorcore"
+    packages = ['ics_mhs_actorcore',
+                'ics_mhs_actorcore.RO',
+                'ics_mhs_actorcore.actorcore'
+                'ics_mhs_actorcore.opscore'
+                'ics_mhs_actorcore.external']
 )
