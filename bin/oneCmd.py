5082aba2a15a779abd10a2d1c596980db9d29c1d
Craig Loomis <cloomis@astro.princeton.edu>
2018-03-30 15:48:54 -0400

    Whitespace only

--- bin/oneCmd.py
+++ bin/oneCmd.py
@@ -22,18 +22,18 @@ class OurActor(actorcore.Actor.Actor):
 
         if configFile is None:
             configFile = os.path.expandvars(os.path.join('$TRON_ACTORCORE_DIR', 'etc', 'oneCmd.cfg'))
-            
+
         self.cmdActor = cmdActor
         self.cmdStr = cmdStr
         self.timelim = timelim
         self.printTimes = printTimes
-        
+
         self.printLevels = {'D':0, '>':0,
                             'I':1, ':':1,
                             'W':2,
                             'F':3, '!':4}
         self.printLevel = self.printLevels[printLevel]
-        
+
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
         actorcore.Actor.Actor.__init__(self, name, 
@@ -49,7 +49,7 @@ class OurActor(actorcore.Actor.Actor):
         fractime = int((now % 1) * 1000)
 
         return "%s.%03d " % (basetime, fractime)
-    
+
     def printResponse(self, resp):
         reply = resp.replyList[-1]
         code = resp.lastCode
@@ -75,7 +75,7 @@ class OurActor(actorcore.Actor.Actor):
                                   callCodes=AllCodes)
 
         reactor.callLater(0, self.cmdr.dispatcher.executeCmd, cmdvar)
-        
+
     def _connectionMade(self):
         """twisted arranges to call this when self.cmdr has been established. 
 
