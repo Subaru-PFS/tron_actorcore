0b67d875f69a25e19bf766e5dd20ff6523309b61
Craig Loomis <cloomis@astro.princeton.edu>
2019-08-15 04:30:09 -0400

    Add timecards.py to give Subaru-compliant time cards.
    startTime = astropy.time.Time.now()
      [ expose ]
    endTime = astropy.time.Time.now()
    
    tc = timecards.TimeCards(startTime)
    tc.end(endTime)
    
    cards = tc.getCards()
    
    cards are an array of fitsio.FITSCard-friendly dicts.

---
+++ python/actorcore/utility/timecards.py
@@ -0,0 +1,156 @@
+import numpy as np
+
+import pytz
+import astropy.time
+import astropy.coordinates
+
+class TimeCards(object):
+    """ Provide FITS time cards for Subaru. """
+
+    def __init__(self, startTime, location="Subaru", timezoneName='HST'):
+        """ Set the starting time of the exposure.
+
+        Args
+        ----
+        startTime : `astropy.time.Time`
+          The start of the exposure. Must be utc
+        """
+
+        if not (isinstance(startTime, astropy.time.Time) and startTime.scale == 'utc'):
+            raise TypeError("startTime must be a UTC astropy.time.Time")
+
+        self.startTime = startTime
+        self.endTime = None
+        self.location = astropy.coordinates.EarthLocation.of_site(location)
+        self.timezoneName = timezoneName
+        self.timezone = pytz.timezone(timezoneName)
+
+    def __str__(self):
+        return f"TimeCards(start={self.startTime} end={self.endTime})"
+
+    def end(self, endTime):
+        """ Set the time of the end of the exposure.
+
+        Args
+        ----
+        endTime : `astropy.time.Time`
+          The end of the exposure. Must be utc
+        """
+        if not (isinstance(endTime, astropy.time.Time) and endTime.scale == 'utc'):
+            raise TypeError("endTime must be a UTC astropy.time.Time")
+
+        self.endTime = endTime
+
+    @staticmethod
+    def dateAndTime(t):
+        t.format = 'isot'
+        return t.isot.split('T')
+
+    def _getCardsForTime(self, timeStamp, suffix, timeComment=""):
+        """ Given a time, generates all the Subaru time cards.
+
+        Args
+        ----
+        timeStamp : `astropy.time.Time`
+          The time to format for.
+        suffix : `str`
+          A string to append to the card names
+        timeComment : `str`
+          A stting to append to the card comment
+
+        Returns
+        -------
+        cards : list of `fitsio.FITSCard` compliant dictionaries.
+
+        """
+        cards = []
+
+        cards.append(dict(name=f"MJD{suffix}",
+                          value=np.round(timeStamp.mjd, 8),
+                          comment=f"[d] MJD {timeComment}"))
+
+        cards.append(dict(name=f"UT{suffix}",
+                          value=self.dateAndTime(timeStamp)[1],
+                          comment=f"[HMS] UT {timeComment}"))
+
+        local = timeStamp.to_datetime(timezone=self.timezone)
+        localValue = local.time().isoformat(timespec='milliseconds')
+        cards.append(dict(name=f"{self.timezoneName}{suffix}",
+                          value=localValue,
+                          comment=f"[HMS] {self.timezoneName} {timeComment}"))
+
+        last = timeStamp.sidereal_time('apparent', self.location.lon)
+        cards.append(dict(name=f"LST{suffix}",
+                          value=last.to_string(sep=':', precision=3),
+                          comment=f"[HMS] LST {timeComment}"))
+
+        return cards
+
+    def getCardsForTime(self, timePoint, addSuffix=True):
+        """ Generate all the cards for the start, middle, or end of an exposure.
+
+        Basically, arranges to call self._getCardsForTime() with appropriate card names.
+
+        Args
+        ----
+        timePoint : {'start', '', 'end'}
+          The time we want cards for.
+
+        """
+        if timePoint == 'start':
+            suffix = "-STR" if addSuffix else ""
+            timestamp = self.startTime
+            comment = "at exposure start"
+        elif timePoint == "end":
+            if self.endTime is None:
+                raise RuntimeError("end time must be known to generate -END or middle time cards")
+            suffix = "-END"
+            timestamp = self.endTime
+            comment = "at exposure end"
+        elif not timePoint:
+            if self.endTime is None:
+                raise RuntimeError("end time must be known to generate -END or middle time cards")
+            suffix = ""
+            timestamp = self.startTime + (self.endTime - self.startTime)/2
+            comment = "at mid-exposure"
+        else:
+            raise ValueError("timePoint must be 'start', 'end', or ''")
+
+        if timePoint in ('end', None) and self.endTime is None:
+            raise RuntimeError("end time must be known to generate -END or middle time cards")
+
+        return self._getCardsForTime(timestamp, suffix, comment)
+
+    def baseCards(self):
+        """ Return the common Subaru FITS time cards.
+
+        Specifically, Subaru is always UTC, and does not want times in DATE-OBS.
+        """
+        cards = []
+
+        date, _ = self.dateAndTime(self.startTime)
+        cards.append(dict(name="TIMESYS", value="UTC", comment="Time System used in the header"))
+        cards.append(dict(name="DATE-OBS", value=date, comment="[YMD] Observation start"))
+
+        return cards
+
+    def getCards(self):
+        """ Get all time cards for an exposure.
+
+        We always return TIMESYS, DATE-OBS, MJD-STR, UT-STR, HST-STR, and LST-STR.
+        If we have an end time, we also gwnerate the cards for the middle and end of the exposure.
+
+        Returns
+        -------
+        cards : list of `fitsio.FITSCard` compliant dictionaries.
+
+        """
+        cards = self.baseCards()
+        if self.endTime is None:
+            cards.extend(self.getCardsForTime('start', addSuffix=False))
+        else:
+            cards.extend(self.getCardsForTime('start'))
+            cards.extend(self.getCardsForTime(''))
+            cards.extend(self.getCardsForTime('end'))
+
+        return cards
