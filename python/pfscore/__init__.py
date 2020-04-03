7bf3b6b3e1cf7f7cefafa913c64aa881890f0d6c
Arnaud Le Fur <arnaud.lefur@lam.fr>
2020-04-03 15:46:42 +0200

    connect gen2 model if necessary

--- python/pfscore/__init__.py
+++ python/pfscore/__init__.py
@@ -1,8 +1,12 @@
-
 def fetchVisitFromGen2(cls, cmd=None):
     """Actually get a new visit from Gen2.
     What PFS calls a "visit", Gen2 calls a "frame".
     """
+    if 'gen2' not in cls.models.keys():
+        cls.addModels(['gen2'])
+        gen = cls.bcast if cmd is None else cmd
+        gen.inform('text="connecting gen2 model"')
+
     ret = cls.cmdr.call(actor='gen2', cmdStr='getVisit', timeLim=10.0, forUserCmd=cmd)
     if ret.didFail:
         raise RuntimeError("Failed to get a visit number in 10s!")
